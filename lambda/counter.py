a58407b8c5288a713ee67264abec3e7142d4fdee
Ravjot Duhra <duhraravjot@gmail.com>
2026-03-26 14:32:02 -0700

    Week 5: API Gateway live, visitor counter working on resume

--- lambda/counter.py
+++ lambda/counter.py
@@ -5,6 +5,19 @@ dynamodb = boto3.resource('dynamodb', region_name='ca-central-1')
 table = dynamodb.Table('cloud-resume-visitor-count')
 
 def lambda_handler(event, context):
+    
+    # Handle CORS preflight
+    if event.get('requestContext', {}).get('http', {}).get('method') == 'OPTIONS':
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, OPTIONS',
+                'Access-Control-Allow-Headers': 'Content-Type'
+            },
+            'body': ''
+        }
+
     # Get current count
     response = table.get_item(Key={'id': 'visitors'})
     count = int(response['Item']['count'])
@@ -20,13 +33,12 @@ def lambda_handler(event, context):
         ExpressionAttributeValues={':val': count}
     )
 
-    # Return count with CORS headers
     return {
         'statusCode': 200,
         'headers': {
             'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET',
-            'Content-Type': 'application/json'
+            'Access-Control-Allow-Methods': 'GET, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type'
         },
         'body': json.dumps({'count': count})
     }
